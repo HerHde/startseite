3b5fdaf9ed49c90f492dc2e9c189b14bff9d04ca
HerHde <admin@herh.de>
2015-12-29 20:05:28 +0100

    Fixed paths.

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-fold -s index.md | gpg2 --clearsign > index.asc
+fold -s _includes/index.md | gpg2 --clearsign > _includes/index.asc
 gpg2 --export --export-options=export-minimal --armor admin@herh.de > herhde.asc
-git add index.md index.asc herhde.asc
-git commit -S
+git add _includes/index.md _includes/index.asc herhde.asc
+jekyll build && git commit -S
